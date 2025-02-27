f0ad59fac9ac86dab52037ee42c01e32d9e371eb
0nlyssslatts <sid36205@mail.ru>
2025-02-27 12:26:38 +0300

    bunch of style and scripts changes

--- public/script.js
+++ public/script.js
@@ -1,5 +1,10 @@
+// FORMS
 const form1 = document.getElementById("contactForm1");
 const form2 = document.getElementById("contactForm2");
+// MOBILE MENU
+const menuToggle = document.getElementById("menu-toggle");
+const navMobile = document.getElementById("mobile-nav");
+let navMobileState = false;
 
 function formHandle(num) {
     return function (event) {
@@ -26,6 +31,23 @@ function formHandle(num) {
             });
     };
 }
-
 form1.addEventListener("submit", formHandle(1));
 form2.addEventListener("submit", formHandle(2));
+
+menuToggle.onclick = function () {
+    navMobileState = !navMobileState;
+    menuToggle.classList.toggle("menu-icon-active");
+    navMobile.classList.toggle("mobile-nav-active");
+};
+
+window.addEventListener("resize", function () {
+    if (navMobileState && window.innerWidth >= 840) {
+        closeMenu();
+    }
+});
+
+function closeMenu() {
+    navMobileState = false;
+    menuToggle.classList.remove("menu-icon-active");
+    navMobile.classList.remove("mobile-nav-active");
+}
